a2ea5291a3518de176a23206f1ea2ed9def018df
John Hellman <acoppero@gmail.com>
2018-01-03 16:34:21 +0100

    Small Change

--- Identity-CodeAlong/Identity-CodeAlong/Migrations/Configuration.cs
+++ Identity-CodeAlong/Identity-CodeAlong/Migrations/Configuration.cs
@@ -30,6 +30,25 @@ namespace Identity_CodeAlong.Migrations
             //    );
             //
 
+            //* Proper way of doing it *//
+             /* RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
+            RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            string[] roleNames = new[] { "Admin", "Member" };
+            foreach (string roleName in roleNames)
+            {
+                if (!context.Roles.Any(r => r.Name == roleName))
+                {
+                    IdentityRole role = new IdentityRole { Name = roleName };
+                    IdentityResult result = roleManager.Create(role);
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    }
+                }
+            }*/
+
+            //* Quick and dirty way of doing it *//
             context.Roles.AddOrUpdate(x=> x.Name, new IdentityRole("Admin"));
 
             UserStore<ApplicationUser> userStore = new 
