9a8e443442500b03fbc04a9f003be394a91b911d
John Hellman <acoppero@gmail.com>
2018-01-03 16:31:11 +0100

    V1.0

--- Identity-CodeAlong/Identity-CodeAlong/Models/AccountViewModels.cs
+++ Identity-CodeAlong/Identity-CodeAlong/Models/AccountViewModels.cs
@@ -70,12 +70,19 @@ namespace Identity_CodeAlong.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
+        [Display(Name = "First Name")]
+        [MinLength(2)]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [MinLength(2)]
         public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FullName { get { return FirstName + " " + LastName; } }
-        public DateTime TimeOfRegistration { get; set; }
-
-
+        
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
